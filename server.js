7068befdc8dc24b4cb7af1426c2461ab6dedd57b
Roshan Rai <rairoshan69883@gmail.com>
2026-04-01 12:14:08 +0530

    Update CORS to allow shopceragresluxe.com

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5173", 
+    "http://localhost:5174", 
+    "https://shopceragresluxe.com", 
+    "https://www.shopceragresluxe.com"
+  ],
   credentials: true
 }));
 app.use(express.json());
